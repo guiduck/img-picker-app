bdbad60d62e3de7f6b3ca868089760279dd2d291
Guilherme <guiduck02@gmail.com>
2020-05-31 23:18:03 -0300

    screen & navigation setup complete

--- App.js
+++ App.js
@@ -1,10 +1,6 @@
 import React from 'react';
-import { View } from 'react-native';
+import PlacesNavigator from './navigation/PlacesNavigator';
 
 export default function App() {
-  return (
-    <View>
-      
-    </View>
-  );
+  return <PlacesNavigator />;
 }
--- navigation/PlacesNavigator.js
+++ navigation/PlacesNavigator.js
@@ -1,11 +1,12 @@
 import { Platform } from 'react-native';
-import { createStackNavigator, createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
 
 import PlacesListScreen from '../screens/PlacesListScreen';
 import PlaceDetailScreen from '../screens/PlaceDetailScreen';
 import NewPlaceScreen from '../screens/NewPlaceScreen';
 import MapScreen from '../screens/MapScreen';
-import colors from '../constants/Colors';
+import Colors from '../constants/Colors';
 
 const PlacesNavigator = createStackNavigator(
   {
