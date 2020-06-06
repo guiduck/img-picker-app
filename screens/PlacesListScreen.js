ed9d87f334f805086736ad9e5897b508696069bf
Guilherme <guiduck02@gmail.com>
2020-06-05 23:27:29 -0300

    outputting a list of places

--- screens/PlacesListScreen.js
+++ screens/PlacesListScreen.js
@@ -3,12 +3,31 @@ import { View, Text, StyleSheet, Platform } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import HeaderButton from '../components/HeaderButton';
+import { FlatList } from 'react-native-gesture-handler';
+import { useSelector } from 'react-redux';
+import PlaceItem from '../components/PlaceItem';
 
 const PlacesListScreen = (props) => {
+  const places = useSelector((state) => state.places.places);
+
   return (
-    <View>
-      <Text>PlacesListScreen</Text>
-    </View>
+    <FlatList
+      data={places}
+      keyExtractor={(item) => item.id}
+      renderItem={(itemData) => (
+        <PlaceItem
+          image={null}
+          title={itemData.item.title}
+          address={null}
+          onSelect={() => {
+            props.navigation.navigate('PlaceDetail', {
+              placeTitle: itemData.item.title,
+              placeId: itemData.item.id,
+            });
+          }}
+        />
+      )}
+    />
   );
 };
 
