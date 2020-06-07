efac1a761d330192928a3041a1bfa784c055c8fa
Guilherme <guiduck02@gmail.com>
2020-06-06 23:37:37 -0300

    accessing the camera. done in android

---
+++ components/ImagePicker.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { View, Button, Text, StyleSheet, Image } from 'react-native';
+import Colors from '../constants/Colors';
+import * as ImagePicker from 'expo-image-picker';
+
+const ImgPicker = (props) => {
+  const takeImageHandler = () => {
+    ImagePicker.launchCameraAsync();
+  };
+
+  return (
+    <View style={styles.imagePicker}>
+      <View style={styles.imagePreview}>
+        <Text>No image picked yet</Text>
+        <Image style={styles.image} />
+      </View>
+      <Button
+        title='Take Image'
+        color={Colors.primary}
+        onPress={takeImageHandler}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  imagePicker: {
+    alignItems: 'center',
+  },
+  imagePreview: {
+    width: '100%',
+    height: 200,
+    marginBottom: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#ccc',
+    borderWidth: 1,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+});
+
+export default ImgPicker;
