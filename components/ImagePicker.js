01127a5cc60e84984659ad56204b1ac7e3f6ff7c
Guilherme <guiduck02@gmail.com>
2020-06-09 00:05:10 -0300

    using stored img uri with state

--- components/ImagePicker.js
+++ components/ImagePicker.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Button, Text, StyleSheet, Image, Alert } from 'react-native';
 import Colors from '../constants/Colors';
 import * as ImagePicker from 'expo-image-picker';
 import * as Permissions from 'expo-permissions';
 
 const ImgPicker = (props) => {
+  const [pickedImage, setPickedImage] = useState('');
+
   const verifyPermissions = async () => {
     const result = await Permissions.askAsync(
       Permissions.CAMERA,
@@ -26,14 +28,24 @@ const ImgPicker = (props) => {
     if (!hasPermission) {
       return;
     }
-    ImagePicker.launchCameraAsync();
+    const image = await ImagePicker.launchCameraAsync({
+      allowsEditing: true,
+      aspect: [16, 9],
+      quality: 0.5,
+    });
+
+    setPickedImage(image.uri);
+    console.log(image);
   };
 
   return (
     <View style={styles.imagePicker}>
       <View style={styles.imagePreview}>
-        <Text>No image picked yet</Text>
-        <Image style={styles.image} />
+        {!pickedImage ? (
+          <Text>No image picked yet</Text>
+        ) : (
+          <Image style={styles.image} source={{ uri: pickedImage }} />
+        )}
       </View>
       <Button
         title='Take Image'
