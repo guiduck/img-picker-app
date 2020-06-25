35368250069c3f22c796cd0b303dd09875d7c465
Guilherme <guiduck02@gmail.com>
2020-06-24 23:51:30 -0300

    showing map preview

--- components/MapPreview.js
+++ components/MapPreview.js
@@ -1,10 +1,35 @@
 import React from 'react';
-import { Image, StyleSheet } from 'react-native';
+import { View, Image, StyleSheet } from 'react-native';
+
+import ENV from '../env';
 
 const MapPreview = (props) => {
-  imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${props.location.lat},${props.location.lng}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:A%7C${props.location.lat},${props.location.lng}&key=AIzaSyDfUwiBUHxDtAbC4rDk_exkMo4_Z9n3mCY`;
+  let imagePreviewUrl;
+
+  if (props.location) {
+    imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${props.location.lat},${props.location.lng}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:A%7C${props.location.lat},${props.location.lng}&key=${ENV.googleApiKey}`;
+  }
+
+  return (
+    <View style={{ ...styles.mapPreview, ...props.style }}>
+      {props.location ? (
+        <Image style={styles.mapImage} source={{ uri: imagePreviewUrl }} />
+      ) : (
+        props.children
+      )}
+    </View>
+  );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  mapPreview: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  mapImage: {
+    width: '100%',
+    height: '100%',
+  },
+});
 
 export default MapPreview;
---
+++ env.js
@@ -0,0 +1,5 @@
+const vars = {
+  googleApiKay: 'AIzaSyDfUwiBUHxDtAbC4rDk_exkMo4_Z9n3mCY',
+};
+
+export default vars;
