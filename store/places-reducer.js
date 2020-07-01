0ed858d8619a7a3047b08f71dfd663fd5ca1c8e5
Guilherme <guiduck02@gmail.com>
2020-06-30 22:11:35 -0300

    stoing the address

--- store/places-reducer.js
+++ store/places-reducer.js
@@ -10,7 +10,15 @@ export default (state = initialState, action) => {
     case SET_PLACES:
       return {
         places: action.places.map(
-          (pl) => new Place(pl.id.toString(), pl.title, pl.imageUri)
+          (pl) =>
+            new Place(
+              pl.id.toString(),
+              pl.title,
+              pl.imageUri,
+              pl.address,
+              pl.lat,
+              pl.lng
+            )
         ),
       };
 
@@ -19,7 +27,10 @@ export default (state = initialState, action) => {
         new Date().toString(),
         action.placeData.id.toString(),
         action.placeData.title,
-        action.placeData.imageUri
+        action.placeData.imageUri,
+        action.placeData.address,
+        action.placeData.coords.lat,
+        action.placeData.coords.lng
       );
       return {
         places: state.places.concat(newPlace),
